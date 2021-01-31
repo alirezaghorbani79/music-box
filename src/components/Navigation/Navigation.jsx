ed6711832411d5e009ebdd33f412b981d807f512
Alireza ghorbani <bounty.1379@gmail.com>
2021-01-31 16:49:09 +0330

    initial Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+
+const Navigation = () => {
+    return (
+        <nav>
+            <h2>Logo</h2>
+            <ul>
+                <li>but</li>
+                <li>but</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation

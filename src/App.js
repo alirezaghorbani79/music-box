28f7ff3dc39b83b194183389cef8f6f1aff96c84
Alireza ghorbani <bounty.1379@gmail.com>
2021-02-01 16:14:57 +0330

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+        <Home />
+
+      </Router>
     </>
   );
 }

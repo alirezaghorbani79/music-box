fde66db2cab9622aa95a0bb5aca8ddabf0b56209
Alireza ghorbani <bounty.1379@gmail.com>
2021-02-03 18:23:17 +0330

    fix navigation button

--- src/components/Navigation/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems.jsx
@@ -5,7 +5,7 @@ import styles from '../../styles/Navigation/NavigationItems.module.scss'
 const NavigationItems = () => {
     return (
         <ul className={styles.Items}>
-            <li><NavLink to="/" exact activeClassName={styles.Active} >Home</NavLink></li>
+            <li><NavLink to="/" exact activeClassName={styles.Active} > Home</NavLink></li>
             <li><NavLink to="/browse" exact activeClassName={styles.Active} >Browse</NavLink></li>
             <li><NavLink to="/library" exact activeClassName={styles.Active} >Library</NavLink></li>
         </ul>

54c47ddd92c78a177292c72b3fd94ddbedd7fddf
Alireza ghorbani <bounty.1379@gmail.com>
2021-03-28 20:44:29 +0430

    add slider to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import Navigation from '../../components/Navigation/Navigation'
+import Slider from '../../components/Sliders/Slider'
 
 const Home = () => {
-    return (
-        <Navigation />
-    )
+  return (
+    <>
+      <Navigation />
+      <Slider />
+    </>
+  )
 }
 
 export default Home

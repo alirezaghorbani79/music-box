0504ffe71991648adbf4c9142640d36bb7f96f3b
Alireza ghorbani <bounty.1379@gmail.com>
2021-03-31 21:34:18 +0430

    add react query provider to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Home from './pages/Home/Home'
 
 const App = () => {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+        refetchOnWindowFocus: false,
+      },
+    },
+  })
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router>
         <Home />
-
       </Router>
-    </>
-  );
+    </QueryClientProvider>
+  )
 }
 
-
-export default App;
+export default App

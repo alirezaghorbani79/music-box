61aea62a7b939eb0af6eef9b506c208bb1d463ff
Alireza ghorbani <bounty.1379@gmail.com>
2021-03-31 21:39:29 +0430

    add genres component to homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Layout from '../../components/Layout/Layout'
-import TopArtists from '../../components/Sliders/TopArtists'
+import TopArtists from '../../components/Carousel/TopArtists'
+import Genres from '../../components/Carousel/Genres'
 
 const Home = () => {
   return (
     <Layout>
+      <Genres />
       <TopArtists />
     </Layout>
   )

1051f16d0fd8c5f880a5a4581effd6d86beb27ca
Alireza ghorbani <bounty.1379@gmail.com>
2021-03-31 12:40:26 +0430

    initial component

---
+++ src/components/Sliders/TopArtists.jsx
@@ -0,0 +1,18 @@
+import styles from '../../styles/Sliders/Styles.module.scss'
+
+
+import gImage from '../../assets/images/genres/0.png'
+import Slider from './Slider'
+
+
+
+const TopArtists = () => {
+    return (
+
+        <section className={styles.box}>
+            <Slider />
+        </section>
+    )
+}
+
+export default TopArtists

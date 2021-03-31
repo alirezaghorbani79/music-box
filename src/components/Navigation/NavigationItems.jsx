534af8597bd057b069040f105b0d1cdb93147f7c
Alireza ghorbani <bounty.1379@gmail.com>
2021-03-31 21:33:32 +0430

    change items name

--- src/components/Navigation/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems.jsx
@@ -5,9 +5,9 @@ import styles from '../../styles/Navigation/NavigationItems.module.scss'
 const NavigationItems = () => {
     return (
         <ul className={styles.Items}>
-            <li><NavLink to="/" exact activeClassName={styles.Active} > Home</NavLink></li>
-            <li><NavLink to="/browse" exact activeClassName={styles.Active} >Browse</NavLink></li>
-            <li><NavLink to="/library" exact activeClassName={styles.Active} >Library</NavLink></li>
+            <li><NavLink to="/" exact activeClassName={styles.Active} >Home</NavLink></li>
+            <li><NavLink to="/category" exact activeClassName={styles.Active} >Category</NavLink></li>
+            <li><NavLink to="/about" exact activeClassName={styles.Active} >About</NavLink></li>
         </ul>
     )
 }

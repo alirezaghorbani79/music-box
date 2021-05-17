0fc7023f551e0dcc1f1e9ec90619951dc06981c0
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:19:10 +0430

    initial button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/Button/Button.module.scss'
+
+const Button = ({ type, children, ...props }) => {
+
+
+
+    return (
+        <div className={[styles.button, styles[type]].join(' ')} {...props}>
+            {children}
+        </div>
+    )
+}
+
+export default Button

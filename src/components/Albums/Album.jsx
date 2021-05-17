bebd6ead2ca8ca489d5c317e86f968192cb84329
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:18:26 +0430

    add link to album cover

---
+++ src/components/Albums/Album.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import styles from '../../styles/Albums/Album.module.scss'
+
+const Album = ({ title, release_date, cover, id }) => {
+  return (
+    <Link to={`/albums/${id}`} >
+      <div className={styles.album}>
+        <img src={cover} alt="Album_cover" />
+        <h5>{title}</h5>
+        <p>Released: {release_date}</p>
+      </div>
+    </Link>
+  )
+}
+
+export default Album

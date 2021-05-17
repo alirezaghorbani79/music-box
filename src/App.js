7e3386ecb6d096c893defafb57acc8568aad61ab
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:20:41 +0430

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 import Home from './pages/Home/Home'
+import Layout from './components/Layout/Layout'
+import Category from './pages/Category/Category'
+import Artist from './pages/Artist/Artist'
+import Album from './pages/Album/Album'
 
 const App = () => {
-
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -17,7 +20,14 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Home />
+        <Switch>
+          <Layout>
+            <Route path="/" exact component={Home} />
+            <Route path="/category" exact component={Category} />
+            <Route path="/artists/:id" component={Artist} />
+            <Route path="/albums/:id" component={Album} />
+          </Layout>
+        </Switch>
       </Router>
     </QueryClientProvider>
   )

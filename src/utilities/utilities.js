11e770fb6bac52d8e2dc3ab481adce18f04706e6
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:21:10 +0430

    initial secToMin function

---
+++ src/utilities/utilities.js
@@ -0,0 +1,10 @@
+const secToMin = (time) => {
+  const minute = Math.floor(time / 60)
+  const second = Math.floor(time % 60)
+
+  if (second < 10) return `${minute}:0${second}`
+
+  return `${minute}:${second}`
+}
+
+export { secToMin }

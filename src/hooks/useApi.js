9cd3ee13d3a659b40bf554c226fc808e861407a3
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:21:50 +0430

    add hooks

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -6,6 +6,36 @@ const useTopArtists = () => {
     return useQuery('top_artists', api.getTopArtists)
 }
 
+const useTopTracks = () => {
+    return useQuery('top_tracks', api.getTopTracks)
+}
+
+const useTopAlbums = () => {
+    return useQuery('top_albums', api.getTopAlbums)
+}
+
+const useArtistInfo = (id) => {
+    return useQuery(['artist_info', id], () => api.getArtistInfo(id))
+}
+
+const useArtistPlaylist = (id) => {
+    return useQuery(['artist_playlist', id], () => api.getArtistPlaylist(id))
+}
+
+const useArtistAlbums = (id) => {
+    return useQuery(['artist_album', id], () => api.getArtistAlbums(id))
+}
+
+const useAlbum = (id) => {
+    return useQuery(['album', id], () => api.getAlbum(id))
+}
+
 export {
-    useTopArtists
+    useTopArtists,
+    useTopTracks,
+    useTopAlbums,
+    useArtistInfo,
+    useArtistPlaylist,
+    useArtistAlbums,
+    useAlbum
 }

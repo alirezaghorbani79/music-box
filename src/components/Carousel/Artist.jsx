434da06fac1b6f8963974e805aa92831907924f1
Alireza ghorbani <bounty.1379@gmail.com>
2021-05-17 15:24:23 +0430

    change some styles

--- src/components/Carousel/Artist.jsx
+++ src/components/Carousel/Artist.jsx
@@ -1,23 +1,17 @@
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import styles from '../../styles/Carousel/Artist.module.scss'
 
 const Artist = ({ name, picture, id }) => {
 
-    const history = useHistory()
-
-    const routeHanler = () => {
-        history.push(`/artists/${id}`)
-    }
-
-
-    return (
-        <div className={styles.card} onClick={routeHanler} >
-            
-            <img className={styles.picture} src={picture} alt="Aritst"/>
-            <h3 className={styles.name}>{name}</h3>
-        </div>
-    )
+  return (
+    <Link to={`/artists/${id}`} >
+      <div className={styles.card}>
+        <img className={styles.picture} src={picture} alt="Aritst" />
+        <h3 className={styles.name}>{name}</h3>
+      </div>
+    </Link>
+  )
 }
 
 export default Artist

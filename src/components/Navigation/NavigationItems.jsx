c7b783832f9c58bb9fa26b5493146fe302aee3e6
Alireza ghorbani <bounty.1379@gmail.com>
2021-06-06 20:17:11 +0430

    change route

--- src/components/Navigation/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems.jsx
@@ -6,7 +6,7 @@ const NavigationItems = () => {
     return (
         <ul className={styles.Items}>
             <li><NavLink to="/" exact activeClassName={styles.Active} >Home</NavLink></li>
-            <li><NavLink to="/Categories" activeClassName={styles.Active} >Category</NavLink></li>
+            <li><NavLink to="/categories" activeClassName={styles.Active} >Category</NavLink></li>
             <li><NavLink to="/about" exact activeClassName={styles.Active} >About</NavLink></li>
         </ul>
     )

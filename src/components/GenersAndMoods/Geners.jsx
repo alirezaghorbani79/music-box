cfc475c6ea3050d36734b112f0870b11abe62990
Alireza ghorbani <bounty.1379@gmail.com>
2021-06-08 21:38:11 +0430

    remove logs

--- src/components/GenersAndMoods/Geners.jsx
+++ src/components/GenersAndMoods/Geners.jsx
@@ -5,24 +5,19 @@ import Genre from './Genre'
 import styles from '../../styles/GenersAndMoods/Genres.module.scss'
 
 const Geners = () => {
-    const genreName = Object.keys(genres)
+  const genreName = Object.keys(genres)
 
-    const items = genresImage.map((el, index) => {
-        const picture = el.default
-        return <Genre picture={picture} genreName={genreName[index]} key={index} />
-    })
+  const items = genresImage.map((el, index) => {
+    const picture = el.default
+    return <Genre picture={picture} genreName={genreName[index]} key={index} />
+  })
 
-    console.log(items)
-
-    return (
-        <div className={styles.box}>
-            <h2>Genres</h2>
-            <div className={styles.items}>
-                {items}
-            </div>
-
-        </div>
-    )
+  return (
+    <div className={styles.box}>
+      <h2>Genres</h2>
+      <div className={styles.items}>{items}</div>
+    </div>
+  )
 }
 
 export default Geners

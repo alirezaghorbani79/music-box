3cc0b1cc79299ef014419261ff462cb9a5579c58
Alireza ghorbani <bounty.1379@gmail.com>
2021-06-11 16:03:45 +0430

    change route

--- src/components/Carousel/Track.jsx
+++ src/components/Carousel/Track.jsx
@@ -4,7 +4,7 @@ import styles from '../../styles/Carousel/Track.module.scss'
 
 const Track = ({ id, name, picture, artist_name, preview }) => {
     return (
-        <Link to={`/album/${id}`} >
+        <Link to={`/albums/${id}`} >
           <div className={styles.card}>
             <img className={styles.picture} src={picture} alt="Aritst" />
             <h3 className={styles.name} >{name}</h3>

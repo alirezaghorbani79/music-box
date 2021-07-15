6bdc1c5122d0021fb4328b1ab564c715680d4057
Alireza ghorbani <bounty.1379@gmail.com>
2021-07-15 12:08:21 +0430

    add music player controler functions

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+import styles from '../../styles/Panel/Panel.module.scss'
+import DesktopPlayer from './DesktopPlayer'
+
+import { ReactComponent as Play } from '../../assets/svg/play.svg'
+import { ReactComponent as Pause } from '../../assets/svg/pause.svg'
+
+
+import m from '../../assets/Masoud Sadeghloo - Shab Ahangi.mp3'
 
 const Panel = () => {
+    const [isPlaying, setIsPlaying] = useState()
+    const audioRef = useRef()
+    const progressRef = useRef()
+
+    const playHandler = () => {
+        isPlaying ? audioRef.current.pause() : audioRef.current.play() 
+        setIsPlaying((prevState) => !prevState)
+    }
+
+    const updateTimeHandler = (event) => {
+        const currentTime = event.target.currentTime
+        const duration = event.target.duration
+        const progressWitdh = currentTime / duration * 100 + '%'
+
+        progressRef.current.style.width = progressWitdh
+    }
+
+    const changeTimeHandler = (newTime) => {
+        const duration = audioRef.current.duration
+        const currentTime = duration * newTime
+
+        audioRef.current.currentTime = currentTime
+    }
+    
+    const playIcon = isPlaying ? Play : Pause
+
+
+    const props = {
+        playHandler, progressRef, changeTimeHandler, playIcon
+    }
+
     return (
-        <div>
-            
+        <div className={styles.panel}>
+            <audio ref={audioRef} src={m} id='audio' onTimeUpdate={updateTimeHandler} />
+
+            <DesktopPlayer {...props} />
         </div>
     )
 }

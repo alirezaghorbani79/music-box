90f062533c7e48e6dd80e0ecd75df8133629a097
Alireza ghorbani <bounty.1379@gmail.com>
2021-07-16 19:33:41 +0430

    add song cover

--- src/components/Panel/InfoBox.jsx
+++ src/components/Panel/InfoBox.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 import styles from '../../styles/Panel/InfoBox.module.scss'
 
-const InfoBox = () => {
+const InfoBox = ({ songInfo }) => {
     return (
         <div className={styles.box}>
-            {/* TODO: add song cover to here */}
+            <img src={songInfo.cover} alt='songCover' />
             <div className={styles.info}>
-                <h4>Song name</h4>
-                <p>Artist name</p>
+                <h4>{songInfo.title}</h4>
+                <p>{songInfo.artist_name}</p>
             </div>
         </div>
     )

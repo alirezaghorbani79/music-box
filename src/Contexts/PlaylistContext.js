a1230e1f57fbfa3d1c4c91d7a44a204b2ce4a931
Alireza ghorbani <bounty.1379@gmail.com>
2021-07-16 15:15:45 +0430

    create reducer and some hooks

--- src/Contexts/PlaylistContext.js
+++ src/Contexts/PlaylistContext.js
@@ -1,23 +1,54 @@
-import { createContext } from 'react'
-
+import { createContext, useContext } from 'react'
+import { useImmerReducer } from 'use-immer'
+import n from './../assets/Masoud Sadeghloo - Shab Ahangi.mp3'
 
 
 const PlaylistStateContext = createContext()
 const PlaylistDispatchContext = createContext()
 
+const initialState = {
+    playlist: null,
+    song: n
+}
+
+const playlistReducer = (state, action) => {
+
+    const { type, payload } = action
+
+    switch (type) {
+        case 'load': {
+            state.playlist = payload.playlist
+            state.song = state.playlist[0]
+        }
+    }
+}
+
 const PlaylistContextProvider = ({ children }) => {
 
-
+    const [playlist, dispatch] = useImmerReducer(playlistReducer, initialState)
     return (
-        <PlaylistStateContext.Provider>
-            <PlaylistDispatchContext.Provider>
+        <PlaylistStateContext.Provider value={playlist} >
+            <PlaylistDispatchContext.Provider value={dispatch} >
                 {children}
             </PlaylistDispatchContext.Provider>
         </PlaylistStateContext.Provider>
     )
 }
 
+const usePlaylistDispatch = () => {
+    const dispatch = useContext(PlaylistDispatchContext)
+    return dispatch
+}
+
+const useSong = () => {
+    const { song } = useContext(PlaylistStateContext)
+    return song
+}
+
+export default PlaylistContextProvider
 
 export {
-    PlaylistContextProvider
+    usePlaylistDispatch,
+    useSong,
+    PlaylistStateContext
 }

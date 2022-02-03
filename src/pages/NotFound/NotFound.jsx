99f84fc98ac7dcfc5931569b3ebedd61d97094db
Alireza Ghorbani <bounty.1379@gmail.com>
2022-02-03 18:53:05 +0330

    add 404 page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from '../../styles/Pages/notFound.module.scss'
+
+const NotFound = () => {
+
+  return (
+    <div className={styles.container} >
+      <h1>404</h1>
+      <h2>Page Not Found!!!</h2>
+    </div>
+  );
+};
+
+export default NotFound;

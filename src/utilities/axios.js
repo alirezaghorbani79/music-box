bbc22506bec546262c9e9e5a380d83ada3447b03
Alireza Ghorbani <48451803+alirezaghorbani79@users.noreply.github.com>
2023-07-09 15:12:19 +0330

    Update api

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://large-underwear-production.up.railway.app/'
+    baseURL: 'https://music-box-xsih.onrender.com/'
 })
 
 export default instance

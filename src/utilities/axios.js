464addd356415d3f7d9dec3e8df5083375f3f2ae
Alireza Ghorbani <bounty.1379@gmail.com>
2024-05-26 18:24:11 +0330

    fix: remove slash from url

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://music-box-xsih.onrender.com/'
+    baseURL: 'https://music-box-xsih.onrender.com'
 })
 
 export default instance
